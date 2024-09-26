e4f00089832ef3429a297cde649bfd71a4ffbfcb
Muhammad Arsalan <arsalan.online24@gmail.com>
2024-09-26 17:45:07 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // This specifies the output directory
-  },
-});
+})
+
+
+
+
