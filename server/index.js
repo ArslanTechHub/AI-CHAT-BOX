9c520513a2b74f728a47bbab3ee6b09a0738d1f3
Muhammad Arsalan <arsalan.online24@gmail.com>
2024-09-26 16:30:07 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
